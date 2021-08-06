c000f835bd3974b6fe70d689e031bbce4d6d9e84
my-1132 <myk1350909@gmail.com>
2021-08-06 12:17:43 +0900

    ログイン機能追加しました

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    todos:[]
+    todos:[],
+    login_user: null,
   },
   mutations: {
     addTodo(state,todo){
@@ -13,6 +15,12 @@ export default new Vuex.Store({
     },
     deleteAction(state,index){
       state.todos.splice(index,1)
+    },
+    setLoginUser(state,user){
+      state.login_user = user
+    },
+    deleteLoginUser(state){
+      state.login_user = null
     }
   },
   actions: {
@@ -24,6 +32,19 @@ export default new Vuex.Store({
     },
     deleteAction({commit},index){
       commit("deleteAction",index)
+    },
+    setLoginUser({commit},user){
+      commit('setLoginUser',user)
+    },
+    login(){
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    },
+    logout(){
+      firebase.auth().signOut()
+    },
+    deleteLoginUser({commit}){
+      commit('deleteLoginUser')
     }
   
   },
