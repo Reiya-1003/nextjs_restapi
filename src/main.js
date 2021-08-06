06d3ebbaf89193eb6a58d02c647a1c959ba38a6f
abechihiro <abe.1.0.2.3.mssp@gmail.com>
2021-08-06 09:28:54 +0900

    更新しました

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import firebase from 'firebase'
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBcsz9Q9eNhtRiPixPvjDqS9EN25UmuRUY",
+    authDomain: "todo-e.firebaseapp.com",
+    projectId: "todo-e",
+    storageBucket: "todo-e.appspot.com",
+    messagingSenderId: "1033440226134",
+    appId: "1:1033440226134:web:c4e6df34573b114cadc4f7",
+    measurementId: "G-KSFLJ4D762"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 new Vue({
   router,
